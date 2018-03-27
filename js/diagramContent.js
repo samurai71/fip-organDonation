cb7933f60657f3589dcc7a992f25a078b2df89e1
Mark Landeryou <marklanderyou@hotmail.com>
2018-03-27 12:27:39 -0400

    updates

---
+++ js/diagramContent.js
@@ -0,0 +1,55 @@
+///Information Storage For Organ Diagram
+var organInfo = {
+		heart: {
+			text : "The heart is one of the most important organs in the body. This organ pumps blood throughout the body providing necessary nutrients and cycling out of harmful waste.",
+
+			headline : "Heart"
+		},
+
+		liver : {
+			text : "The liver is one of the largest organs in the body.  It has multitude of functions including: filtering out toxins from blood, producing digestive liquid and storing energy in the form of a sugar called glycogen.",
+
+			headline : "Liver"
+		},
+
+		intestines : {
+			text : "The small intestine is a long, highly intricate tube in the digestive system that absorbs about 90% of the nutrients from the food we eat.",
+
+			headline : "Small Intestines"
+		},
+
+		kidneys : {
+			text : "Kidneys filter the blood, preventing an excess of waste and fluids in the body. They also manage electrolytes and create both red blood cells and hormones, important in regulating blood pressure.",
+
+			headline : "Kidneys"
+		},
+		
+		
+			pancreas : {
+			text : "This organ is in charge of the creation of enzymes which are used to break down food in the lower intestine.  The pancreas is also in charge of regulating the body’s sugar levels by the creation of insulin.",
+
+			headline : "Pancreas"	
+			},
+		
+			
+			lungs : {
+			text : "Essential for life, lungs allow for the filtering and removal of harmful gases and for the collection of oxygen – essential for cell energy and growth.",
+
+			headline : "Lungs"	
+			},
+				
+			bone : {
+			text : "Made from living tissue, bones are the framework of the body. Bones protect our organs and systems from harm.",
+
+			headline : "Bones"	
+			},
+			
+		eyes : {
+			text : "Eyes capture light, images and movement and send that information to the brain. Eyes allow us to see the world and what's happening around us.",
+
+			headline : "Eyes"	
+			}		
+		
+		
+		
+	};
